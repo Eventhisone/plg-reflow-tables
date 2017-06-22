a3db64cb68adbe3023d3ad1521b93b313e0d4a05
Zach Turner <Zach@Daniel.local>
2017-06-22 13:48:09 -0700

    A Joomla plugin that makes tables reflow when the screen size is small

---
+++ reflow-tables.php
@@ -0,0 +1,19 @@
+<?php
+#-------------------------------------------------------------
+# Copyright (C) 2017 by wwwebweavers.com
+# Homepage   : wwwebweavers.com
+# Author     : Zach
+# Email      : eventhisone@gmail.com
+# Version    : 1.0.0
+# License    : http://www.gnu.org/copyleft/gpl.html GNU/GPL
+#-------------------------------------------------------------
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+
+$document = JFactory::getDocument();
+
+$document->addStyleSheet($baseurl.'plugins/content/reflow-tables/reflow-tables.css');
+
+?>
